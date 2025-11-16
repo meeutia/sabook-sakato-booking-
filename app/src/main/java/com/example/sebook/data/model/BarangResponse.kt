3733ebbb2878c29d5154a3b5b54018c8e7942efb
SistriMahira <145315051+SistriMahira@users.noreply.github.com>
2025-11-17 00:22:05 +0700

    Update BarangResponse.kt

--- app/src/main/java/com/example/sebook/data/model/BarangResponse.kt
+++ app/src/main/java/com/example/sebook/data/model/BarangResponse.kt
@@ -1,2 +1,17 @@
 package com.example.sebook.data.model
 
+data class BarangResponse(
+    val success: Boolean,
+
+    val message: String,
+
+    val barang: List<BarangItem>
+)
+
+data class BarangItem(
+    val id_barang: String,
+
+    val nama_barang: String,
+
+    val stok_tersedia_saat_ini: String
+)
